937e6582fc3a3f5ce67b059bb13d76c9e88d6a62
chunkof <chunkoffset@gmail.com>
2015-11-02 00:14:15 +0900

    変数名 xxx_by_min -> xxx_by_m

--- chunkofUtility.js
+++ chunkofUtility.js
@@ -32,21 +32,21 @@ chunkofUt = {};
     // min
     var min_h = Number(min_time.substr(0,2));
     var min_m = Number(min_time.substr(3,2));
-    var min_by_min = min_h*60 + min_m;
+    var min_by_m = min_h*60 + min_m;
 
     // max
     var max_h = Number(max_time.substr(0,2));
     var max_m = Number(max_time.substr(3,2));
-    var max_by_min = max_h*60 + max_m;
+    var max_by_m = max_h*60 + max_m;
 
     // now
     var date_obj = new Date();
     var now_h  = date_obj.getHours();
     var now_m  = date_obj.getMinutes();
-    var now_by_min = now_h*60 + now_m;
+    var now_by_m = now_h*60 + now_m;
     
     // Judge
-    if ((min_by_min<=now_by_min) && (now_by_min<=max_by_min)){
+    if ((min_by_m<=now_by_m) && (now_by_m<=max_by_m)){
         return true;
     }
     return false;
